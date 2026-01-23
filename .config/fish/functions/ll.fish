e0ccea1bac5803f8d259f782959a6ab6a87b5ba6
Tom Verelst <664136+tomverelst@users.noreply.github.com>
2026-01-23 12:33:22 +0100

    fish: group dirs first with ll

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps='eza -a -1 --icons' --description 'ls list'
-  eza -a -1 --icons $argv;
+function ll --wraps='eza -a -1 --icons --group-directories-first' --description 'ls list'
+  eza -a -1 --icons --group-directories-first $argv;
 end
