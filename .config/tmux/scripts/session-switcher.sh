245ed9cfd835d636ab2464f93bd0c14e30cecbf7
Tom Verelst <664136+tomverelst@users.noreply.github.com>
2026-04-01 10:34:29 +0200

    feat(tmux): ✨ open session switcher in popup

--- .config/tmux/scripts/session-switcher.sh
+++ .config/tmux/scripts/session-switcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Fuzzy session/window switcher with live pane preview.
 tmux list-windows -a -F '#{session_name}:#{window_index} #{window_name} (#{pane_current_command})' \
-  | fzf-tmux -d20 \
+  | fzf \
     --preview 'tmux capture-pane -t $(echo {} | cut -d" " -f1) -p' \
     --preview-window=right:60% \
   | cut -d' ' -f1 \
