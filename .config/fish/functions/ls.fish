e4d6f9921304065c83854107527d0ffaa5bcbd4f
Tom Verelst <664136+tomverelst@users.noreply.github.com>
2026-09-14 10:42:07 +0200

    fish: fix ls

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps=exa --wraps=lsd --wraps='eza --icons' --description 'alias ls=eza --icons'
-  eza --icons $argv; 
+function ls --wraps=eza --description 'alias ls=eza --icons=auto'
+  eza --icons=auto $argv
 end
